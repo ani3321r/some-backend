ab77827f4cb490358d2fefcd1d2498c88a2586db
Aniruddha Roy <65581823+ani3321r@users.noreply.github.com>
2024-02-15 23:27:13 +0530

    controller bugs fixed

--- Project/src/utils/cloudinary.js
+++ Project/src/utils/cloudinary.js
@@ -13,6 +13,7 @@ const uploadOnCloudinary = async (localFilePath) => {
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
+    fs.unlink(localFilePath);
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath); //remove the unwanted temporary files if upload fails
